99a2ba320e5b843d97ea52322c22e171a58abca0
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-06-11 11:26:05 +0700

    fix pivot table

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import { fileURLToPath, URL } from "node:url";
+import inject from "@rollup/plugin-inject";
 import pkg from "./package.json";
 
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    inject({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -22,5 +31,8 @@ export default defineConfig({
   },
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
+  },
+  optimizeDeps: {
+    include: ["jquery"],
   },
 });
